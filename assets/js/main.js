333fa821b7fcf83918d18fa97736629474c8c3c0
Rowan <rowan.copeland@gmail.com>
2020-02-12 14:42:44 +0000

    map markers, assoicated with dropdown country  now appearing on map when selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,16 @@
 //MAP LOAD//
-
-const mymap = L.map('mapid').setView([39, -80.5], 6);
-// const marker = L.marker([0, 0]).addTo(mymap);
-
+const myMap = L.map('mapid').setView([39, -80.5], 6);
 const attribution =
     '&copy; <a href="https://www.openstreetmap.org/copyright">"Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>';
+const tileUrl = "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(myMap);
 
-let tileUrl = "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
-let tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(mymap);
 
 //MAP MARKERS//
 
 //api load//
-
 const api_url = "https://opensky-network.org/api/states/all"
-
 
 async function getData() {
     let response = await fetch(api_url);
@@ -24,16 +19,14 @@ async function getData() {
     let countryList = [];
     let mapData = [];
 
-// Push countries and remove duplicates
-
+    // Push countries and remove duplicates
     for (let i = 0; i < state.length; i++) {
         if (countryList.includes(state[i][2]) || state[i][2] === "") { }
         else {
             countryList.push(state[i][2]);
         }
     }
-// Sort Alphabetically
-    
+    // Sort Alphabetically
     countryList.sort(function (country1, country2) {
         if (country1 > country2) {
             return 1;
@@ -43,8 +36,7 @@ async function getData() {
         }
     });
 
-// Add countries to dropdown
-
+    // Add countries to dropdown
     let select = document.getElementById("select");
     for (let i = 0; i < countryList.length; i++) {
         let option = document.createElement("OPTION"),
@@ -54,26 +46,74 @@ async function getData() {
         select.insertBefore(option, select.lastChild);
     }
 
-//Search countries
-    document.getElementById("select").addEventListener("change", selectChanged)
+    // Search countries
+    document.getElementById("select").addEventListener("change", selectChanged);
     function selectChanged() {
-        let mapData = [];
+        mapData=[];
         var countrySearch = document.getElementById("select").value;
-            for(let i = 0; i < state.length; i++){
-                for(let j = 0; j <= [i].length; j++){
-                    if(state[i][2] === countrySearch){ 
-                        mapData.push(state[i]);
-            } 
+        for (let i = 0; i < state.length; i++) {
+            if (state[i][2] === countrySearch) {
+                mapData.push(state[i]);
+            }
+        }
+        for (let i = 0; i < mapData.length; i++) {
+            if (mapData[i][5] != null || mapData[i][6] != null) {
+                let marker = L.marker([mapData[i][6], mapData[i][5]]).addTo(myMap);
+            }
+        }
     }
-    }
-    console.log(mapData);
 }
-}
-getData();
+
+    // if (document.getElementById("trueButton").checked){
+    //     var trueValue = document.getElementById("trueButton").value;
+    //     for (let i = 0; i < state.length; i++) {
+    //         if (state[i][8] === trueValue) {
+    //             mapData.push(state[i]);
+    //         }
+    //     }
+    //     console.log(mapData);
+    // };    
+    getData();
+
+
+// let dataObject = mapData.map(item => {
+//         return {
+//             icao24: item[0],
+//             callsign: item[1],
+//             origin_country: item[2],
+//             time_position: item[3],
+//             last_contact: item[4],
+//             longitude: item[5],
+//             latitude: item[6],
+//             baro_altitude: item[7],
+//             on_ground: item[8],
+//             velocity: item[9],
+//             true_track: item[10],
+//             vertical_rate: item[11],
+//             sensors: item[12],
+//             geo_altitude: item[13],
+//             squawk: item[14],
+//             spi: item[15],
+//             position_source: item[16]
+//         }
+
+// });
+
+//     function mapPointer(){
+//     for(let i = 0; i <= mapData.length; i++){
+//     L.marker([dataObject[i].latitude, dataObject[i].longitude]).addTo(myMap);
+//     }
+// }
+// let mapMarkers = [];
+//     for (let i = 0; i >= mapData.length; i++){
+//             console.log(mapMarkers[i[5]])
+
+//     }
+// console.log(mapData);
 
 // for(let i = 0; i < 10; i++){
-//     L.polyline(([dataObject[i].latitude, dataObject[i].longitude]), {color: 'red'}).addTo(mymap);
-//     // L.marker([dataObject[i].latitude, dataObject[i].longitude]).addTo(mymap);
+//     L.polyline(([dataObject[i].latitude, dataObject[i].longitude]), {color: 'red'}).addTo(myMap);
+//     // L.marker([dataObject[i].latitude, dataObject[i].longitude]).addTo(myMap);
 //     // latitude: dataObject[i].latitude,
 //     // longitude: dataObject[i].longitude
 //     }
