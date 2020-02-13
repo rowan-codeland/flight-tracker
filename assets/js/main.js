df724290075488906583381076234ab46728534f
Rowan <rowan.copeland@gmail.com>
2020-02-13 15:53:22 +0000

    restructured API to await statement to allow data to be returned outside function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//MAP LOAD//
+//Loading map
 const myMap = L.map('mapid').setView([39, -80.5], 6);
 const attribution =
     '&copy; <a href="https://www.openstreetmap.org/copyright">"Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>';
@@ -6,86 +6,133 @@ const tileUrl = "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(myMap);
 
-
-//MAP MARKERS//
+var state = [];
+console.log(state);
+//html button function - This was moved outside of getData(); as it needed to be read by the HTML @ index.html (line 33). Works if you comment out lines 14-18 and use uncomment console.log on line 19. Needs to access array called state from within the getData function
+function displayMyData() {
+    let testingThis = document.getElementsByName("ground");
+    if (testingThis[0].checked) {
+        for (let i = 0; i < state.length; i++) {
+            if (state[i][8] === true) {
+                console.log(state[i]);
+            }
+        }
+        // console.log("hello this is a test")
+    }
+}
 
 //api load//
 const api_url = "https://opensky-network.org/api/states/all"
 
-async function getData() {
+async function testGetData() {
     let response = await fetch(api_url);
     let data = await response.json();
-    let state = data.states;
-    let countryList = [];
-    let mapData = [];
+    return data.states;
+}
 
-    // Push countries and remove duplicates
-    for (let i = 0; i < state.length; i++) {
-        if (countryList.includes(state[i][2]) || state[i][2] === "") { }
-        else {
-            countryList.push(state[i][2]);
+function getData() {
+    //   let response = await fetch(api_url);
+    // let data = await response.json();
+
+    //drill down to array containing flights (console.log(state); returns all flights)//
+    testGetData().then(function (data) {
+        state = data;
+        let countryList = [];
+
+        //mapData - everychaning array that filtered data get's pushed to. this array is used to load flights to map//
+        let mapData = [];
+
+
+        // Remove duplicate countries from API and push to countryList array
+        for (let i = 0; i < state.length; i++) {
+            if (countryList.includes(state[i][2]) || state[i][2] === "") { }
+            else {
+                countryList.push(state[i][2]);
+            }
         }
-    }
-    // Sort Alphabetically
-    countryList.sort(function (country1, country2) {
-        if (country1 > country2) {
-            return 1;
+        // Sort countries alphabetically
+        countryList.sort(function (country1, country2) {
+            if (country1 > country2) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        });
+
+        // Add countries to dropdown
+        let select = document.getElementById("select");
+        for (let i = 0; i < countryList.length; i++) {
+            let option = document.createElement("OPTION"),
+                txt = document.createTextNode(countryList[i]);
+            option.appendChild(txt);
+            option.setAttribute("value", countryList[i]);
+            select.insertBefore(option, select.lastChild);
         }
-        else {
-            return -1;
+
+        // Search countries from dropdown
+        document.getElementById("select").addEventListener("change", selectChanged);
+        function selectChanged() {
+            mapData = [];
+            let countrySearch = document.getElementById("select").value;
+            for (let i = 0; i < state.length; i++) {
+                if (state[i][2] === countrySearch) {
+                    mapData.push(state[i]);
+                }
+                //Add flights with matching country to map
+            }
+            for (let i = 0; i < mapData.length; i++) {
+                if (mapData[i][5] != null || mapData[i][6] != null) {
+                    let marker = L.marker([mapData[i][6], mapData[i][5]]).addTo(myMap);
+                }
+            }
         }
+
+        displayMyData();
     });
 
-    // Add countries to dropdown
-    let select = document.getElementById("select");
-    for (let i = 0; i < countryList.length; i++) {
-        let option = document.createElement("OPTION"),
-            txt = document.createTextNode(countryList[i]);
-        option.appendChild(txt);
-        option.setAttribute("value", countryList[i]);
-        select.insertBefore(option, select.lastChild);
-    }
-
-    // Search countries
-    document.getElementById("select").addEventListener("change", selectChanged);
-    function selectChanged() {
-        mapData=[];
-        var countrySearch = document.getElementById("select").value;
-        for (let i = 0; i < state.length; i++) {
-            if (state[i][2] === countrySearch) {
-                mapData.push(state[i]);
-            }
-        }
-        for (let i = 0; i < mapData.length; i++) {
-            if (mapData[i][5] != null || mapData[i][6] != null) {
-                let marker = L.marker([mapData[i][6], mapData[i][5]]).addTo(myMap);
-            }
-        }
-    }
-
-    //Flights on the ground
-    document.getElementById("truebutton").addEventListener("click", clickedTrue);
-    function clickedTrue(){
-        for (let i = 0; i < state.length; i++) {
-            if (state[i][8] === true) {
-                mapData.push(state[i]);
-            }
-        }
-    }
-
-    //Flights in the air
-    document.getElementById("falsebutton").addEventListener("click", clickedFalse);
-    function clickedFalse(){
-        for (let i = 0; i < state.length; i++) {
-            if (state[i][8] === false) {
-                mapData.push(state[i]);
-            }
-        }
-        console.log(mapData);
-    }    
 }
 getData();
 
+
+//----Slider Element----//
+
+// let mySlider = new rSlider({
+//     target: '#sampleSlider',
+//     values: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015],
+//     range: true,
+//     tooltip: true,
+//     scale: true,
+//     labels: true,
+//     set: [2010, 2013]
+// });
+
+//------ ⬇⬇⬇ ROUGH WORK - DON'T DEletE UNTIL FINAL COMMIT ⬇⬇⬇--------//
+
+
+// Display flights based on in on ground or not
+
+//     function displayMyData(){
+//         let testingThis = document.getElementByName("ground");
+//         for(let i = 0; i <= testingThis.length; i++){
+//             if (testingThis[0].checked){
+//                 console.log("hello")
+//         for (let i = 0; i < state.length; i++) {
+//             if (state[i][8] === true) {
+//                 console.log(state[i])
+//             }
+//     }
+
+//         else if (testingThis[1].checked){
+//                 for (let i = 0; i < state.length; i++) {
+//                     if (state[i][8] === false) {
+//                         console.log(state[i])
+//                     }
+//     }
+// }
+// }
+
+//Map Data//
 
 // let dataObject = mapData.map(item => {
 //         return {
@@ -109,85 +156,3 @@ getData();
 //         }
 
 // });
-
-//     function mapPointer(){
-//     for(let i = 0; i <= mapData.length; i++){
-//     L.marker([dataObject[i].latitude, dataObject[i].longitude]).addTo(myMap);
-//     }
-// }
-// let mapMarkers = [];
-//     for (let i = 0; i >= mapData.length; i++){
-//             console.log(mapMarkers[i[5]])
-
-//     }
-// console.log(mapData);
-
-// for(let i = 0; i < 10; i++){
-//     L.polyline(([dataObject[i].latitude, dataObject[i].longitude]), {color: 'red'}).addTo(myMap);
-//     // L.marker([dataObject[i].latitude, dataObject[i].longitude]).addTo(myMap);
-//     // latitude: dataObject[i].latitude,
-//     // longitude: dataObject[i].longitude
-//     }
-
-
-// document.getElementById("demo").innerHTML = "You sekected:" + xyz;
-// console.log(xyz);
-// setInterval(getData, 1000);
-
-
-
-
-// var latLong = state
-
-// for(let i=0; i < 10; i++){
-//         state[i] = { 
-//             long: i+1,
-//             lat: i+1
-//         };
-//     }
-// };
-// console.log(latLong);
-
-// Example
-
-// function doSomething(country, something, else){
-//     var whatToReturn = []
-//     const response = await fetch(api_url);
-//     const data = await response.json();
-
-//     const state = data.states;
-//     for data.states {
-//         for agurments{
-//             if arguements == data[field]{
-
-//             }
-//         }
-//     }
-
-// }
-
-// document ready add listener to HTML input/select elements.
-
-//API Map
-
-//    // const dataObject = state.map(item => {
-    //     return {
-    //         icao24: item[0],
-    //         callsign: item[1],
-    //         origin_country: item[2],
-    //         time_position: item[3],
-    //         last_contact: item[4],
-    //         longitude: item[5],
-    //         latitude: item[6],
-    //         baro_altitude: item[7],
-    //         on_ground: item[8],
-    //         velocity: item[9],
-    //         true_track: item[10],
-    //         vertical_rate: item[11],
-    //         sensors: item[12],
-    //         geo_altitude: item[13],
-    //         squawk: item[14],
-    //         spi: item[15],
-    //         position_source: item[16]
-    //     }
-    // });
